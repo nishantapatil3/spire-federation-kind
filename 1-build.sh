72e6b1e585d307638eed0ab012fbaffff55ca05e
Nishant Patil <nishantpatil@Nishants-MacBook-Air.local>
2022-08-27 12:16:42 -0700

    Reformat dockerfile

--- 1-build.sh
+++ 1-build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-set -e
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-(cd src/broker-webapp && CGO_ENABLED=0 GOOS=linux go build -v -o $DIR/docker/broker-webapp/broker-webapp)
-(cd src/stock-quotes-service && CGO_ENABLED=0 GOOS=linux go build -v -o $DIR/docker/stock-quotes-service/stock-quotes-service)
+mkdir -p build
+(cd src/broker-webapp && CGO_ENABLED=0 GOOS=linux go build && mv broker-webapp ../../build/)
+(cd src/stock-quotes-service && CGO_ENABLED=0 GOOS=linux go build && mv stock-quotes-service ../../build/)
 
 echo "**** Finished building binaries ****"
--- Makefile
+++ Makefile
@@ -4,9 +4,8 @@ build:
 	${PWD}/1-build.sh
 
 docker-build:
-	docker build docker/broker-webapp -t docker.io/nishantapatil3/broker-webapp:latest
-	docker build docker/stock-quotes-service -t docker.io/nishantapatil3/stock-quotes-service:latest
+	docker build . -f docker/Dockerfile.broker-webapp -t docker.io/nishantapatil3/broker-webapp:latest
+	docker build . -f docker/Dockerfile.stock-quotes-service -t docker.io/nishantapatil3/stock-quotes-service:latest
 
 clean:
-	rm docker/broker-webapp/broker-webapp
-	rm docker/stock-quotes-service/stock-quotes-service
+	rm -rf build
